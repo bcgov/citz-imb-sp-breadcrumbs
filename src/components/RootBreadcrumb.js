41cbb0cd8cf5c0b9a0bed3ad926aea2fb3a55c1e
Toews <Scott.Toews@gov.bc.ca>
2020-02-11 15:48:49 -0800

    Daily Commit

--- src/components/RootBreadcrumb.js
+++ src/components/RootBreadcrumb.js
@@ -1,47 +1,63 @@
-import React, { Component } from 'react'
-import SiteBreadcrumb from './SiteBreadcrumb'
-import ListBreadcrumb from './ListBreadcrumb'
-import FolderBreadcrumb from './FolderBreadcrumb'
-import NameBreadcrumb from './NameBreadcrumb'
-import { Breadcrumbs, Link } from '@material-ui/core'
-import axios from 'axios'
+import React, { Component } from "react";
+import SiteBreadcrumb from "./SiteBreadcrumb";
+import ListBreadcrumb from "./ListBreadcrumb";
+import FolderBreadcrumb from "./FolderBreadcrumb";
+import NameBreadcrumb from "./NameBreadcrumb";
+import { Breadcrumbs, Link } from "@material-ui/core";
+import Axios from "axios";
 
-export default class RootBreadcrumb extends Component {
-    constructor(props) {
-        super(props)
+export class RootBreadcrumb extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            siteAbsoluteUrl: ""
+    this.state = {
+      siteName: "Root"
+    };
+  }
+
+  componentDidMount() {
+    Axios.get(this.props.pageContext.siteAbsoluteUrl + "/_api/web")
+      .then(response => {
+        console.log("response", response);
+        this.setState({
+          siteName: response.data.Title
+        });
+      })
+      .catch(error => {
+        console.groupCollapsed("Error Details");
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
         }
-    }
+        console.log(error.config);
+        console.groupEnd();
+      });
+  }
 
-    handleClick(event){
-        event.preventDefault();
-        console.info('You clicked a breadcrumb.');
-    }
-
-    componentDidMount(){
-        axios
-        .get('../_api/web')
-        .then(response => {
-            console.log(response)
-        })
-        // this.setState = {
-        //     siteAbsoluteUrl: _spPageContextInfo.siteAbsoluteUrl
-        // }
-    }
-
-    render() {
-        return (
-            <Breadcrumbs aria-label="breadcrumb" separator="\">
-                <Link color="primary" href="#" onClick={this.handleClick}>
-                    ROOT
-                </Link>
-                <SiteBreadcrumb />
-                <ListBreadcrumb />
-                <FolderBreadcrumb />
-                <NameBreadcrumb />
-            </Breadcrumbs>
-        )
-    }
+  render() {
+    return (
+      <Breadcrumbs aria-label="breadcrumb" separator="\">
+        <Link color="primary" href={this.props.pageContext.siteAbsoluteUrl}>
+          <div>{this.state.siteName}</div>
+        </Link>
+        <SiteBreadcrumb pageContext={this.props.pageContext} />
+        <ListBreadcrumb pageContext={this.props.pageContext} />
+        <FolderBreadcrumb pageContext={this.props.pageContext} />
+        <NameBreadcrumb pageContext={this.props.pageContext} />
+      </Breadcrumbs>
+    );
+  }
 }
+
+export default RootBreadcrumb;
--- src/components/SiteBreadcrumb.js
+++ src/components/SiteBreadcrumb.js
@@ -1,17 +1,62 @@
-import React, { Component} from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from '@material-ui/core'
+import Axios from 'axios'
 
 export class SiteBreadcrumb extends Component {
-    handleClick = event => {
-        event.preventDefault();
-        console.info('You clicked a breadcrumb.');
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            siteName: "Site"
+        }
+
+        console.log("pageContext", this.props.pageContext)
+
+    }
+
+    componentDidMount() {
+        const relativeUrl = this.props.pageContext.webServerRelativeUrl.replace(this.props.pageContext.siteServerRelativeUrl + '/', '')
+
+        const nodes = relativeUrl.split('/')
+
+        nodes.foreach(node)
+
+        Axios.all()
+
+
+        // Axios.get(this.props.pageContext.siteAbsoluteUrl + "/_api/web")
+        //   .then(response => {
+        //     console.log("response", response);
+        //     this.setState({
+        //       siteName: response.data.Title
+        //     });
+        //   })
+        //   .catch(error => {
+        //     console.groupCollapsed("Error Details");
+        //     if (error.response) {
+        //       // The request was made and the server responded with a status code
+        //       // that falls out of the range of 2xx
+        //       console.log(error.response.data);
+        //       console.log(error.response.status);
+        //       console.log(error.response.headers);
+        //     } else if (error.request) {
+        //       // The request was made but no response was received
+        //       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        //       // http.ClientRequest in node.js
+        //       console.log(error.request);
+        //     } else {
+        //       // Something happened in setting up the request that triggered an Error
+        //       console.log("Error", error.message);
+        //     }
+        //     console.log(error.config);
+        //     console.groupEnd();
+        //   });
     }
 
     render() {
         return (
-            <Link color="primary" href="#" onClick={this.handleClick}>
-                SITE
-            </Link>
+            <div></div>
         )
     }
 }
